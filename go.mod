b01a809d847ae42c5fafd9f85a621b20d1e83a54
Nick <nicholas.m.ricci@gmail.com>
2024-08-07 01:36:57 +0000

    test2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/MatrixFox621/e621-rec
 
-go 1.22.5
+go 1.22.1
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/MatrixFox621/e621-rec/internal"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println(internal.Test().(type))
 }
